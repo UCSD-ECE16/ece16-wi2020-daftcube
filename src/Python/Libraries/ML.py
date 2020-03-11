2d3fedd47095468d628c5cb846829825dd800f52
daftcube <owen.bartolf@Gmail.com>
2020-03-10 22:21:35 -0700

    Moved filtering to its own function

--- src/Python/Libraries/ML.py
+++ src/Python/Libraries/ML.py
@@ -54,9 +54,80 @@ class ML:
                 heartrate_data = data_array[:,4]
                 
                 # Filter Data
-                heartrate_data = self.hr.detrend(heartrate_data, 4)
-                heartrate_data = self.hr.low_pass(heartrate_data, .065)
-                heartrate_data = self.hr.normalize_signal(heartrate_data)
+                heartrate_data = self.filter_ml_data(heartrate_data)
+                
+                # Append to list_data. Slice off any additional values
+                if self.list_data.size > 0:
+                    self.list_data = np.vstack((self.list_data, heartrate_data[:500]))
+                else:
+                    self.list_data = heartrate_data[:500]
+                
+                # Append subject id to list_sub
+                self.list_sub = np.append(self.list_sub, int(sub_id))
+                
+                # Extract reference heartrate from file, then add to list_ref.
+                heartrate_ref_string = sub_file.split("_")
+                heartrate_ref = int(heartrate_ref_string[len(heartrate_ref_string) - 1].split(".")[0])
+                
+                self.list_ref = np.append(self.list_ref, heartrate_ref)
+    
+    # Trains the HR model using the dataset in the given directory.
+    def train_hr_model(self, train_ids):
+        train_data = np.array([])
+        hold_out_data = np.array([])
+        
+        hold_out_sub = train_ids[0] # For now we’ll hold out the first training subject
+        
+        for index, sub_id in enumerate(self.list_sub, 0):
+            if sub_id != hold_out_sub:
+                train_data = np.concatenate((train_data, self.list_data[index]))
+            else:
+                hold_out_data = np.concatenate((hold_out_data, self.list_data[index]))
+        
+        self.gmm = GMM(n_components=2).fit(train_data.reshape(-1, 1))
+        test_pred = self.gmm.predict(hold_out_data.reshape(-1, 1))
+        
+        # plt.plot(test_pred)
+        plt.plot(test_pred[1500:2500])
+        plt.plot(hold_out_data[1500:2500])
+        
+        plt.show()
+        
+        print(self.calc_hr(hold_out_data[1500:2500], 50))
+        
+    # The Machine-learning specific algorithm to calculate the heart rate
+    # Returns the heart rate
+    def calc_hr(self, s, fs):
+        analyzed_data = self.gmm.predict(s.reshape(-1,1))
+        plt.plot(analyzed_data)
+        plt.show()
+        return -1
+    
+    
+    # Runs a test of the machine learning model using the data in the given directory.
+    def test_hr_model(self, directory):
+        # Fetch unique IDs in directory
+        all_files = glob.glob(directory + "*.csv")
+        unique_ids = list(set([file.split("\\")[2][0:2] for file in set(all_files)]))
+        
+        self.list_test_data = np.array([])
+        self.list_test_sub = np.array([])
+        self.list_test_ref = np.array([])
+        
+        # print(unique_ids)
+        
+        for sub_id in unique_ids:
+                
+            # For each unique ID, get all sub files.
+            sub_files = glob.glob(directory + "\\" + sub_id + "_*_*.csv")
+            
+            for sub_file in sub_files:
+                # Load and splice data
+                data_array = np.genfromtxt(sub_file, delimiter=",")
+                heartrate_data = data_array[:,4]
+                
+                # Filter Data
+                heartrate_data = self.filter_ml_data(heartrate_data)
                 
                 # Append to list_data. Slice off any additional values
                 if self.list_data.size > 0:
@@ -76,47 +147,13 @@ class ML:
         
         print("Done")
     
-    # Trains the HR model using the dataset in the given directory.
-    def train_hr_model(self, train_ids):
-        train_data = np.array([])
-        hold_out_data = np.array([])
+    # The filtering function for ML stuff
+    def filter_ml_data(self, heartrate_data):
+        heartrate_data = self.hr.detrend(heartrate_data, 4)
+        heartrate_data = self.hr.low_pass(heartrate_data, .065)
+        heartrate_data = self.hr.normalize_signal(heartrate_data)
+        return heartrate_data
         
-        hold_out_sub = train_ids[0] # For now we’ll hold out the first training subject
-        
-        for index, sub_id in enumerate(self.list_sub, 0):
-            if sub_id != hold_out_sub:
-                print(index)
-                train_data = np.concatenate((train_data, self.list_data[index]))
-            else:
-                print(index)
-                hold_out_data = np.concatenate((hold_out_data, self.list_data[index]))
-        
-        self.gmm = GMM(n_components=2).fit(train_data.reshape(-1, 1))
-        test_pred = self.gmm.predict(hold_out_data.reshape(-1, 1))
-        
-        # plt.plot(test_pred)
-        plt.plot(test_pred[1000:1500])
-        plt.plot(hold_out_data[1000:1500])
-        
-        plt.show()
-        
-        print(self.calc_hr(hold_out_data[3000:3500], 50))
-        
-    # The Machine-learning specific algorithm to calculate the heart rate
-    # Returns the heart rate
-    def calc_hr(self, s, fs):
-        analyzed_data = self.gmm.predict(s.reshape(-1,1))
-        plt.plot(analyzed_data)
-        plt.show()
-        
-        return self.hr.calc_heart_rate_time(s, fs)
-    
-    # Runs a test of the machine learning model using the data in the given directory.
-    def test_hr_model(self, directory):
-        pass
-    
-    def plot_training_data(self, directory):
-        pass
 
 ml = ML()
 
